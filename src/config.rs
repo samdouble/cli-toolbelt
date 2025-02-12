6fa8bc9d249fe22dacb77026cc7386b17ec0e226
samdouble <samleafs@protonmail.com>
2025-02-11 21:35:56 -0500

    feat: read TOML config file

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,23 @@
-impl Config {
-    pub fn load(path: &Path)->Result<Self>{
-        Ok(serde::from_str(&fs::read_to_string(path)?)?)
-    }
+use std::fs;
+use std::process::exit;
+use toml;
+
+pub fn read_toml(filename: &str) {
+    let contents = match fs::read_to_string(filename) {
+        Ok(c) => c,
+        Err(_) => {
+            eprintln!("Could not read file `{}`", filename);
+            exit(1);
+        }
+    };
+
+    let data: toml::Table = match toml::from_str(&contents) {
+        Ok(d) => d,
+        Err(_) => {
+            eprintln!("Unable to load data from `{}`", filename);
+            exit(1);
+        }
+    };
+
+    println!("{:?}", data);
 }
